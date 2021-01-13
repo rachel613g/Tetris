919cf00ebc120d5cc4be681c3f3bd7905b9936a8
Jennifer Komendant <jennifer.komendant@gmail.com>
2021-01-12 22:27:07 -0500

    added grid to view

--- src/main/java/tetris/TetrisView.java
+++ src/main/java/tetris/TetrisView.java
@@ -6,11 +6,12 @@ import java.awt.*;
 public class TetrisView extends JComponent {
 
     TetrisGame game;
+    Grid grid;
 
-    public TetrisView(TetrisGame game) {
+    public TetrisView(TetrisGame game, Grid grid) {
         this.game = game;
+        this.grid = grid;
     }
-
 
     TetrisShapes tetrisShapes = new TetrisShapes();
     Point[][][] point = tetrisShapes.getShapes();
@@ -18,17 +19,17 @@ public class TetrisView extends JComponent {
     public static final int CELL_SIZE = 25;
 
     private void drawPiece(Graphics g) {
-        g.setColor(game.getColorArray()[game.getCurrentPiece()]);
+        g.setColor(grid.getColorArray()[game.getCurrentPiece()]);
         for (Point p : point[game.getCurrentPiece()][game.getRotation()]) {
             g.fillRect((p.x + game.getPoint().x) * BORDER_CELL_SIZE, (game.getPoint().y + p.y) * BORDER_CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
     }
 
     public void paintComponent(Graphics g) {
-        g.fillRect(0, 0, BORDER_CELL_SIZE * game.getWidth(), BORDER_CELL_SIZE * (game.getHeight()-1));
-        for (int i = 0; i < game.getWidth(); i++) {
-            for (int j = 0; j < game.getHeight()-1; j++) {
-                g.setColor(game.getBoard()[i][j]);        //null
+        g.fillRect(0, 0, BORDER_CELL_SIZE * grid.getWidth(), BORDER_CELL_SIZE * (grid.getHeight()-1));
+        for (int i = 0; i < grid.getWidth(); i++) {
+            for (int j = 0; j < grid.getHeight()-1; j++) {
+                g.setColor(grid.getBoard()[i][j]);        //null
                 g.fillRect(BORDER_CELL_SIZE * i, BORDER_CELL_SIZE * j, CELL_SIZE, CELL_SIZE);
             }
         }
